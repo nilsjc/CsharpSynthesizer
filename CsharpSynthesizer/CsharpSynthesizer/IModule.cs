e52e74f156a767ca3b3e176d2cb67cc4d6074020
Nils Edvardsson <nils@bazooka.se>
2017-06-29 11:48:51 +0200

    Interface for creating a synth module

---
+++ CsharpSynthesizer/CsharpSynthesizer/IModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpSynthesizer
+{
+    interface IModule
+    {
+        float Signal(float amp, int SampPoint, List<float> CVs);
+    }
+}
